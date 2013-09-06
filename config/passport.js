1e0fe106965fffffa0af00d9f2100c7a3219ea7f
Hack Reactor Students <github@hackreactor.com>
2013-09-06 16:09:48 -0700

    More additions to passport for authentication

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 var mongoose        = require('mongoose'),
     User            = mongoose.model('User'),
     LocalStrategy   = require('passport-local').Strategy;
-    RedditStrategy  = require('passport-reddit').Strategy;
 
 module.exports = function(app, config) {
   var passport = app.get('passport');
@@ -11,15 +10,17 @@ module.exports = function(app, config) {
     function(username, password, done) {
       User.findOne({ username: username }, function(err, user) {
         if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
+        if (!user) { return done(null, false, { message: 'Unknown user'}); }
+        if (!user.verifyPassword(password)) { return done(null, false, {message: 'Invalid password'}); }
         return done(null, user);
       });
     }
   ));
 
   passport.serializeUser(function(user, done) {
-
+    User.findOne({ user:user }, function(err, user) {
+      done(null, user.sessionId);
+    })
   });
 
   passport.deserializeUser(function(sessionId, done) {
@@ -27,11 +28,12 @@ module.exports = function(app, config) {
       done(err, user);
     });
   });
-}
+};
 
 var findBySessionId = function(sessionId, fn) {
   User.findOne({ sessionId: sessionId }, function(err, user) {
-    if (err) { return done(err); }
-    if (!user) { return done(null, false); }
+    if (err) { return fn(err); }
+    if (!user) { return fn(null, false); }
+    return fn(null, user);
   })
-}
+};
