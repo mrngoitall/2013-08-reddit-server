9a23ff8e7659d6d7162ea4387c9f2cf5a296f6d8
Hack Reactor Students <github@hackreactor.com>
2013-09-06 11:11:51 -0700

    Added User schema and model

--- config/db.js
+++ config/db.js
@@ -1,10 +1,25 @@
 var mongoose    = require('mongoose'),
     Schema      = mongoose.Schema;
 
+
 module.exports = function(app, config) {
   var db = mongoose.connect(config.db);
 
   // Setup database and UserSchema
+
+  db.on('error', console.error.bind(console, 'connection error: '));
+
+  db.once('open', function() {
+
+    var userSchema = mongoose.Schema({ 
+      username: String,
+      password: String,
+      salt: String
+    });
+
+    var User = mongoose.model('User', userSchema);
+
+  });
 
   return db;
 }
